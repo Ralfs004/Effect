4ec62177ee86446caf733509ad60a398ffc7aef8
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 09:52:36 +0300

    k

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDo from "./ToDo.js"
 
 function App() {
+
+  const toDo = {
+    UserId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+  <ToDo />
+  
     </div>
   );
 }
---
+++ src/ToDo.js
@@ -0,0 +1,11 @@
+function ToDo() {
+return(
+    <>
+    <p>UserId: 1</p>
+    <p>id: 1</p>
+    <p>title: "delectus aut autem"</p>
+    <p>completed: false</p>
+    </>
+)
+    }
+    export default ToDo;
